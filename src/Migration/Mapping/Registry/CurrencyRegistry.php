f3eb4c6b22aff3028b3294380cb406a7acda511a
Krispin Lütjann <k.luetjann@shopware.com>
2019-10-31 16:55:11 +0100

    PT-10842 - Add countries, languages and currencies to registries

--- src/Migration/Mapping/Registry/CurrencyRegistry.php
+++ src/Migration/Mapping/Registry/CurrencyRegistry.php
@@ -5,22 +5,13 @@ namespace Swag\MigrationMagento\Migration\Mapping\Registry;
 class CurrencyRegistry extends AbstractMappingRegistry
 {
     protected static $mapping = [
-        'EUR' => [
-            'symbol' => '€',
-            'name' => 'Euro',
+        'CHF' => [
+            'symbol' => 'CHF',
+            'name' => 'Swiss franc',
             'translations' => [
-                'de-DE' => 'Euro',
-                'en-GB' => 'Euro',
-                'en-US' => 'Euro',
-            ],
-        ],
-        'USD' => [
-            'symbol' => '$',
-            'name' => 'US dollar',
-            'translations' => [
-                'de-DE' => 'US-Dollar',
-                'en-GB' => 'US dollar',
-                'en-US' => 'US dollar',
+                'de-DE' => 'Schweizer Franken',
+                'en-GB' => 'Swiss franc',
+                'en-US' => 'Swiss franc',
             ],
         ],
         'DKK' => [
@@ -30,6 +21,78 @@ class CurrencyRegistry extends AbstractMappingRegistry
                 'de-DE' => 'Dänische Krone',
                 'en-GB' => 'Danish krone',
                 'en-US' => 'Danish krone',
+            ],
+        ],
+        'EUR' => [
+            'symbol' => '€',
+            'name' => 'Euro',
+            'translations' => [
+                'de-DE' => 'Euro',
+                'en-GB' => 'Euro',
+                'en-US' => 'Euro',
+            ],
+        ],
+        'GBP' => [
+            'symbol' => '£',
+            'name' => 'Pound sterling',
+            'translations' => [
+                'de-DE' => 'Pfund Sterling',
+                'en-GB' => 'Pound sterling',
+                'en-US' => 'Pound sterling',
+            ],
+        ],
+        'JPY' => [
+            'symbol' => '¥',
+            'name' => 'Japanese yen',
+            'translations' => [
+                'de-DE' => 'Japanische Yen',
+                'en-GB' => 'Japanese yen',
+                'en-US' => 'Japanese yen',
+            ],
+        ],
+        'NOK' => [
+            'symbol' => 'kr',
+            'name' => 'Norwegian krone',
+            'translations' => [
+                'de-DE' => 'Norwegische Krone',
+                'en-GB' => 'Norwegian krone',
+                'en-US' => 'Norwegian krone',
+            ],
+        ],
+        'PLN' => [
+            'symbol' => 'zł',
+            'name' => 'Polish zloty',
+            'translations' => [
+                'de-DE' => 'Polnische Zloty',
+                'en-GB' => 'Polish zloty',
+                'en-US' => 'Polish zloty',
+            ],
+        ],
+        'RUB' => [
+            'symbol' => '₽',
+            'name' => 'Russian ruble',
+            'translations' => [
+                'de-DE' => 'Russischer Rubel',
+                'en-GB' => 'Russian ruble',
+                'en-US' => 'Russian ruble',
+            ],
+        ],
+        'SEK' => [
+            'symbol' => 'kr',
+            'name' => 'Swedish krona',
+            'translations' => [
+                'de-DE' => 'Schwedische Krone',
+                'en-GB' => 'Swedish krona',
+                'en-US' => 'Swedish krona',
+            ],
+        ],
+        'USD' => [
+            'symbol' => '$',
+            'name' => 'US dollar',
+            'translations' => [
+                'de-DE' => 'US-Dollar',
+                'en-GB' => 'US dollar',
+                'en-US' => 'US dollar',
             ],
         ],
     ];
--- src/Migration/Mapping/Registry/LanguageRegistry.php
+++ src/Migration/Mapping/Registry/LanguageRegistry.php
@@ -5,14 +5,32 @@ namespace Swag\MigrationMagento\Migration\Mapping\Registry;
 class LanguageRegistry extends AbstractMappingRegistry
 {
     protected static $mapping = [
+        'cs-CZ' => [
+            'name' => 'Czech',
+        ],
+        'da-DK' => [
+            'name' => 'Danish',
+        ],
         'de-DE' => [
             'name' => 'German',
         ],
         'en-GB' => [
             'name' => 'English',
         ],
+        'en-US' => [
+            'name' => 'English',
+        ],
         'fr-FR' => [
             'name' => 'France',
+        ],
+        'fi-FI' => [
+            'name' => 'Finnish',
+        ],
+        'nl-NL' => [
+            'name' => 'Dutch',
+        ],
+        'pl-PL' => [
+            'name' => 'Polish',
         ],
     ];
 }
