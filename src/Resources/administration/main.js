86d67f0b687c1ad866b3ccdb8923cb3c9383f428
Krispin Lütjann <k.luetjann@shopware.com>
2019-11-15 10:56:04 +0100

    NTR - Fix new plugin directory structure

--- src/Resources/administration/main.js
+++
@@ -1,3 +0,0 @@
-import './src/component/profile/magento/local/swag-migration-profile-magento-local-credential-form';
-import './src/component/profile/magento/local/swag-migration-profile-magento19-local-credential-form';
-import './src/init/translation.init';
