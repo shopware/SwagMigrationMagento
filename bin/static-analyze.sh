3faed95a16b5bf251d5640aefb883153eaaf9211
Michael Telgmann <mt@shopware.com>
2020-09-08 14:35:19 +0200

    MIG-77 - Increase PHPStan level with introducing a baseline file

--- bin/static-analyze.sh
+++ bin/static-analyze.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+php "`dirname \"$0\"`"/phpstan-config-generator.php
 composer dump-autoload
-php ../../../dev-ops/analyze/vendor/bin/phpstan analyze --configuration phpstan.neon --autoload-file=../../../vendor/autoload.php src
+php ../../../dev-ops/analyze/vendor/bin/phpstan analyze --configuration phpstan.neon --autoload-file=../../../vendor/autoload.php src tests
 
 # Return if phpstan returns with error
 if [ $? -eq 1 ]
