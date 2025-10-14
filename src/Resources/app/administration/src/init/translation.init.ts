3c43450db9a31d76b13c379b42590a10b79b9922
Marcel Brode <m.brode@shopware.com>
2025-10-14 14:06:03 +0200

    feat(translation): Rename translation file to enable language fallbacks
    fixes shopware/shopware#12815

---
+++ src/Resources/app/administration/src/init/translation.init.ts
@@ -0,0 +1,8 @@
+/**
+ * @deprecated tag:v11.0.0 - With Shopware v6.8.0 - `translation.init.ts` will be removed to use automatic language loading with language layer support
+ */
+import deMagentoSnippets from '../app/snippet/de.json';
+import enMagentoSnippets from '../app/snippet/en.json';
+
+Shopware.Locale.extend('de-DE', deMagentoSnippets);
+Shopware.Locale.extend('en-GB', enMagentoSnippets);
